91fde6bdd823fd56c1144bc23733a2abe6c38b7c
NIKOLAOS77 <nbalotis@gmail.com>
2022-05-02 09:51:27 +0300

    second commit

--- App.js
+++ App.js
@@ -1,18 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet,  View,  Button, FlatList } from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
 
+  const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
+
+
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: goalTitle }
+    ]);
+  };
+
+const removeGoalHandler = goalId =>{
+  setCourseGoals(currentGoals => {
+    return currentGoals.filter((goal) => goal.id !==goalId);
+  });
+}
+
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Course Goal"
-          style={styles.input}
-        />
-        <Button title="ADD" />
-      </View>
+      <Button title="Add New Goal" onPress={() => setIsAddMode(true)}/>
+      <GoalInput visible={isAddMode} onAddGoal={addGoalHandler} />
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={itemData => <GoalItem id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value} />}
+      />
 
     </View>
   );
@@ -20,20 +39,11 @@ export default function App() {
 
 const styles = StyleSheet.create({
   screen: {
-    padding: 50
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    width: '80%',
-    justifyContent: 'space-between',
-    alignItems: 'stretch',
-    justifyContent: 'center',
-    alignItems: 'center'
+    
+    padding: 30
+   
+    
 
-  },
-  input:{
-     width: '80%', 
-     borderColor: 'black', 
-     borderWidth: 1, 
-     padding: 3 }
+  }
+
 });
